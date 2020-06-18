9a4413bba8ea6594f14fd24f6c7b3946a63dcdce
mr.funkster <mr.funksters@gmail.com>
2020-06-18 16:32:16 +0300

    Adding a clean bin function

--- script/product_count.js
+++ script/product_count.js
@@ -1,15 +1,48 @@
 const addToCartButtons = document.querySelectorAll('.button2');
-let productCount = document.querySelector('.product-count');
-let popupWindow = document.querySelector('.popup-window');
-let popupCloseButton = document.querySelector('.popup-button');
-let popupProductCount = document.querySelector('.popup-message p');
-let productCountBin = document.querySelector('.bin-anotation');
+let productCount       = document.querySelector('.product-count');
+let popupWindow        = document.querySelector('.popup-window');
+let popupCloseButton   = document.querySelector('.popup-button');
+let popupProductCount  = document.querySelector('.popup-message p');
+let productCountBin    = document.querySelector('.bin-anotation p');
+let binAnotation       = document.querySelector('.bin-anotation');
+let headerBar          = document.querySelector('.header');
+let binIcon            = document.querySelector('.bin');
+let cleanRecycleBtn    = document.querySelector('.bin-anotation .popup-button');
+let cleanPopupAgree    = document.getElementById('bin-clear-agree');
+let cleanPopupCancel   = document.getElementById('bin-clear-cancel');
+let cleanBinMessage    = document.getElementById('bin-message');
+let scrollPos          = 0;
+
 
 for (let addToCartButton of addToCartButtons) {
+    cleanRecycleBtn.addEventListener('click', function() {
+        cleanRecycleBtn.classList.add('white-hover')
+        cleanBinMessage.classList.add('visible');
+        binAnotation.classList.add('visible');
+        cleanPopupAgree.addEventListener('click', function() {
+            let binProductCount = 0;
+            productCount.innerHTML = binProductCount;
+            cleanRecycleBtn.style.display = 'none';
+            productCountBin.innerHTML = `У Вашій корзині ще відсутні товари.`;
+            cleanBinMessage.classList.remove('visible');
+            let timer = setTimeout(function() {
+                binAnotation.classList.remove('visible');
+                cleanRecycleBtn.classList.remove('white-hover');
+            }, 2000);
+        });
+        cleanPopupCancel.addEventListener('click', function() {
+            cleanBinMessage.classList.remove('visible');
+            let timer = setTimeout(function() {
+                binAnotation.classList.remove('visible');
+                cleanRecycleBtn.classList.remove('white-hover');
+            }, 2000);
+        });
+    });
     addToCartButton.addEventListener('click', function() {
         let productTittle = addToCartButton.parentElement.parentElement.querySelector('.product-title p').innerHTML;
         let binProductCount = +productCount.innerHTML;
         binProductCount++;
+        cleanRecycleBtn.style.display = 'block';
         let word;
         if(binProductCount === 1) {
             word = "товар";
@@ -18,12 +51,27 @@ for (let addToCartButton of addToCartButtons) {
         } else if(binProductCount >= 5) {
             word = "товарів"
         }
+        cleanRecycleBtn.addEventListener('click', function() {
+            binProductCount = 0;
+        })
         productCount.innerHTML = binProductCount;
         productCountBin.innerHTML = `Зараз у Вашій корзині ${binProductCount} ${word}.`;
-        popupWindow.classList.add('popup-window-active');
+        popupWindow.classList.add('visible');
         popupProductCount.innerHTML = `Товар "${productTittle}" успішно додано в корзину. Всього в корзині ${binProductCount} ${word}.`;
         popupCloseButton.addEventListener('click', function() {
-            popupWindow.classList.remove('popup-window-active');
+            popupWindow.classList.remove('visible');
         });
     });
-}
+};
+
+
+window.addEventListener('scroll', function() {
+    scrollPos = window.scrollY;
+    if (scrollPos > 400) {
+        headerBar.style.backgroundColor = 'rgba(255, 255, 255, .98)';
+        binIcon.style.borderColor = '#2c71b8';
+    } else if (scrollPos <= 400) {
+        headerBar.style.backgroundColor = 'rgba(255, 255, 255, .75)';
+        binIcon.style.borderColor = 'white';
+    }    
+});
